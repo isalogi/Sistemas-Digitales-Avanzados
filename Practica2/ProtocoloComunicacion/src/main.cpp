f5c0e0ec5a4fee915adbbc974a654c30811399ca
Isabel Cristina <ilopezg1@eafit.edu.co>
2018-08-16 19:17:01 -0500

    cambios profe

--- Practica2/ProtocoloComunicacion/src/main.cpp
+++ Practica2/ProtocoloComunicacion/src/main.cpp
@@ -12,19 +12,53 @@ void setup()
     pinMode(11, OUTPUT);
     pinMode(10, OUTPUT);
     pinMode(9, OUTPUT);
-
-    analogWrite(11, 0);
-    analogWrite(10, 0);
-    analogWrite(9, 0);
 }
 
-void loop()
-{
+class TempSensor {
+    public:
+        TempSensor(uint8_t);
+        void update();
+        float getTemp();
+    private:
+        uint8_t pin;
+        uint16_t buffer[10] = {0,0,0,0,0,0,0,0,0,0};
+        long timer;
+}; 
+
+TempSensor::TempSensor(uint8_t _pin){
+    pin = _pin;
+    timer = millis();
+}
+
+void TempSensor::update(){
+    if(millis()-timer > 500){
+        timer = millis();
+        for(int i=9; i>-1; i--){
+            this->buffer[i] = this->buffer[i-1];
+        }
+        this->buffer[0] = analogRead(this->pin);
+    }
+}
+
+
+float TempSensor::getTemp(){
+    float value = 0;
+    for(int i=0; i<10; i++){
+        value += float(this->buffer[i]);
+    }
+    return (value/(10)) * (5.0/1023) * 1;
+}
+
+TempSensor sensor = TempSensor(A0);
+
+void loop() {
+
+    sensor.update();
+
     int analogTemp = protocol.analogInpRead(0);
     uint8_t temp = protocol.calcTemp(analogTemp);
     protocol.initBuffer(temp);
     Serial.write(protocol.inpBuffer,3);
-    delay(10000);
 
     if (Serial.available() > 0)
     {
@@ -39,7 +73,6 @@ void loop()
             }
         }
         protocol.addData(readInp);
-        delay(10000);
 
         if (protocol.readCounter == 3)
         {

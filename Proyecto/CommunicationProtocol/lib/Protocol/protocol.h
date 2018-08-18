cfe0cd14922b8cf57bc65789207d5a267e9ebd3c
Isabel Cristina <ilopezg1@eafit.edu.co>
2018-08-18 16:13:22 -0500

    inicio protocolo

---
+++ Proyecto/CommunicationProtocol/lib/Protocol/protocol.h
@@ -0,0 +1,13 @@
+#ifndef protocol_h //Evita importar dos veces la libreria
+#define protocol_h
+
+#include <Arduino.h>
+//Archivo primera en minuscula y la clase en mayuscula
+class Protocol
+{
+  public:
+    Protocol();
+
+  private:
+};
+#endif

8f0fb3971a4759399a133845877b4d94d8ca3bae
Isabel Cristina <ilopezg1@eafit.edu.co>
2018-08-19 21:35:10 -0500

    sensor

--- Proyecto/CommunicationProtocol/src/main.cpp
+++ Proyecto/CommunicationProtocol/src/main.cpp
@@ -15,6 +15,8 @@ void setup()
     pinMode(d1, OUTPUT);
     pinMode(d2, OUTPUT);
     pinMode(pin, OUTPUT);
+    pinMode(trig, OUTPUT);
+    pinMode(echo, INPUT);
 
     digitalWrite(d1, LOW);
     digitalWrite(d2, LOW);
@@ -27,15 +29,13 @@ void setup()
 void loop()
 {
     //buffer a escribir en el primer serial
-    uint8_t inpBufferActuador[5] = {0x7e, 1, pin, 80, 0};
-    //procesar sensor
-    uint8_t inpBufferSensor[5] = {0x7e, 0, pin, 0, 0};
+    uint8_t inpBuffer[5] = {0x7e, 0, trig, 0, 0};
 
     //calculamos el checksum según los datos de buffer
-    inpBufferActuador[4] = protocol.createChecksum(inpBufferActuador);
+    inpBuffer[4] = protocol.createChecksum(inpBuffer);
 
     //escribimos el buffer con los datos de entrada
-    mySerial.write(inpBufferActuador, 5);
+    mySerial.write(inpBuffer, 5);
 
     if (Serial.available() > 0)
     {
@@ -48,7 +48,7 @@ void loop()
         //comparamos el checksum que me llegó vs el que construí
         if (protocol.compareChecksum(inpChecksum))
         {
-            if (protocol.buffer[1] == 0) //actuador o sensor
+            if (protocol.buffer[1] == 1) //actuador o sensor
             {
 
                 switch (protocol.buffer[2])
@@ -68,10 +68,21 @@ void loop()
             {
                 switch (protocol.buffer[2])
                 {
-                case 6:
-                    digitalWrite(d1, HIGH);
-                    digitalWrite(d2, LOW);
-                    digitalWrite(protocol.buffer[2], protocol.buffer[3]);
+                case 8:
+                    long duration, distanceCm;
+
+                    digitalWrite(protocol.buffer[2], LOW); //para generar un pulso limpio ponemos a LOW 4us
+                    delayMicroseconds(4);
+                    digitalWrite(protocol.buffer[2], HIGH); //generamos Trigger (disparo) de 10us
+                    delayMicroseconds(10);
+                    digitalWrite(protocol.buffer[2], LOW);
+
+                    duration = pulseIn(echo, HIGH); //medimos el tiempo entre pulsos, en microsegundos
+
+                    distanceCm = duration * 10 / 292 / 2; //convertimos a distancia, en cm
+
+                    Serial.print("Distancia: ");
+                    Serial.println(distanceCm);
                     break;
 
                 default:

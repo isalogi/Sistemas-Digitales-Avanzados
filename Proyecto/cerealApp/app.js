4ac55fa4f2653621c45bd1f84842835620af7c93
Isabel Cristina <ilopezg1@eafit.edu.co>
2018-09-14 19:47:33 -0500

    pagina

--- Proyecto/cerealApp/app.js
+++ Proyecto/cerealApp/app.js
@@ -1,41 +1,26 @@
-var createError = require('http-errors');
+// load things
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// set the view engine to ejs
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('common'));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+var index = require('./routes/index')
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/', index)
 
-module.exports = app;
+
+app.listen(8080);
+console.log('8080 is the magic port');
--- Proyecto/cerealApp/routes/index.js
+++ Proyecto/cerealApp/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+//var arduino = require('../driver/arduino')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+	res.render('../views/index');
 });
 
 module.exports = router;
